17b0870faa32d1fdcd2c4ea79522284f99d9fbc5
sontungN <s3979348@rmit.edu.vn>
2026-01-05 19:59:07 +0700

    feat: refactor Kafka configuration for payment service

--- CompanyPaymentService/src/main/java/rmit/saintgiong/paymentservice/common/kafka/KafkaConsumerConfig.java
+++ CompanyPaymentService/src/main/java/rmit/saintgiong/paymentservice/common/kafka/KafkaConsumerConfig.java
@@ -1,9 +1,10 @@
-package rmit.saintgiong.profileservice.common.kafka;
+package rmit.saintgiong.paymentservice.common.kafka;
 
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -14,7 +15,7 @@ import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
-import rmit.saintgiong.profileapi.internal.common.type.KafkaTopic;
+import rmit.saintgiong.shared.type.KafkaTopic;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,27 +23,34 @@ import java.util.Map;
 @Configuration
 @EnableKafka
 public class KafkaConsumerConfig {
-    
+
+    @Value("${kafka.kafka-host-url}")
+    private String kafkaHostUrl;
+
+    @Value("${kafka.schema-registry-host-url}")
+    private String schemaRegistryHostUrl;
+
     @Bean
     public ConsumerFactory<String, Object> profileConsumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "profile-service-consumer-group");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaHostUrl);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "payment-service-consumer-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
 
         // Config Schema Registry
-        props.put("schema.registry.url", "http://schema-registry:9091");
+        props.put("schema.registry.url", schemaRegistryHostUrl);
 
+        // 6. CỰC KỲ QUAN TRỌNG: Dòng này bảo nó map về đúng class Java (BirdAvro)
         // Nếu thiếu dòng này, nó sẽ trả về GenericRecord và lại gây lỗi khác.
         props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
 
-        // Cho phép đọc từ đầu nếu không tìm thấy offset (tránh lỗi offset out of range)
+        // 7. Cho phép đọc từ đầu nếu không tìm thấy offset (tránh lỗi offset out of range)
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        
+
         return new DefaultKafkaConsumerFactory<>(props);
     }
-    
+
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Object> profileKafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
@@ -50,19 +58,21 @@ public class KafkaConsumerConfig {
         return factory;
     }
 
-//    @Bean
-//    public ConcurrentMessageListenerContainer<String, Object> replyContainer(ConsumerFactory<String, Object> consumerFactory) {
-//        ContainerProperties containerProperties = new ContainerProperties(KafkaTopic.COMPANY_REGISTRATION_REPLY_TOPIC);
-//        containerProperties.setGroupId("profile-service-reply-group");
-//
-//        return new ConcurrentMessageListenerContainer<>(consumerFactory, containerProperties);
-//    }
-//
-//    @Bean
-//    public ReplyingKafkaTemplate<String, Object, Object> replyingKafkaTemplate(
-//            ProducerFactory<String, Object> pf,
-//            ConcurrentMessageListenerContainer<String, Object> replyContainer) {
-//
-//        return new ReplyingKafkaTemplate<>(pf, replyContainer);
-//    }
+    @Bean
+    public ConcurrentMessageListenerContainer<String, Object> replyContainer(ConsumerFactory<String, Object> consumerFactory) {
+        ContainerProperties containerProperties = new ContainerProperties(
+                KafkaTopic.SUBSCRIPTION_PAID_TOPIC_REPLIED
+        );
+        containerProperties.setGroupId("payment-service-reply-group");
+
+        return new ConcurrentMessageListenerContainer<>(consumerFactory, containerProperties);
+    }
+
+    @Bean
+    public ReplyingKafkaTemplate<String, Object, Object> replyingKafkaTemplate(
+            ProducerFactory<String, Object> pf,
+            ConcurrentMessageListenerContainer<String, Object> replyContainer) {
+
+        return new ReplyingKafkaTemplate<>(pf, replyContainer);
+    }
 }
