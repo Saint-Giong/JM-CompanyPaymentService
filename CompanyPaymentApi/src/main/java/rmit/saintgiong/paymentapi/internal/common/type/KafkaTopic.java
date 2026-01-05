34de91b9358cbc9d04de471d81b99884f04d5915
sontungN <s3979348@rmit.edu.vn>
2026-01-05 19:53:56 +0700

    feat: configure payment API settings and update package structure

--- CompanyPaymentApi/src/main/java/rmit/saintgiong/paymentapi/internal/common/type/KafkaTopic.java
+++
@@ -1,9 +0,0 @@
-package rmit.saintgiong.profileapi.internal.common.type;
-
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-public final class KafkaTopic {
-    public static final String COMPANY_REGISTRATION_REQUEST_TOPIC = "JM_COMPANY_REGISTRATION";
-    public static final String COMPANY_REGISTRATION_REPLY_TOPIC = "JM_COMPANY_REGISTRATION_REPLIED";
-}
